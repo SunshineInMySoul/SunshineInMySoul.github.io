93f75960d1ba0e69d6af7999d2f49d25650c1628
RainbowMoonbeams <meaghan.rea@gmail.com>
2017-09-13 10:16:52 -0400

    sun fades in and out

---
+++ SunFadeInAndOut/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".btn1").click(function(){
+        $("div").fadeOut(700)
+    });
+    $(".btn2").click(function(){
+        $("div").fadeIn(1000);
+    });
+});
